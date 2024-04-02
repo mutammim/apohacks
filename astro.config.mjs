ca953a806548be54c42333854d995b1d4ced0484
Mutammim <58490040+mutammim@users.noreply.github.com>
2024-04-02 11:40:08 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  redirects: {
-    '/': 'https://apocalypse.hackclub.com'
-  },
   output: "server",
   adapter: vercel({
     webAnalytics: {
